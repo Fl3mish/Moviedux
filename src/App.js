f46ffa6af46d35d2405caba3b3a1fd8931e4faa3
Fl3mish <mitch.pauwels@proton.me>
2024-08-08 21:52:04 +0100

    init commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <h1>Welcome to Moviedux</h1>
+      </header>
+      <footer className="footer">
+        <p className="footer-text">Footer content here</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
